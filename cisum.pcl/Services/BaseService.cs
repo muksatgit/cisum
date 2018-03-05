4d1082790f296938292df7aabd0d6d145fa470e7
Mukesh Sharma 3 <msharma@sapient.com>
2018-03-05 12:32:36 +0530

    Removed unused code

--- cisum.pcl/Services/BaseService.cs
+++ cisum.pcl/Services/BaseService.cs
@@ -44,20 +44,6 @@ namespace cisum.Services
             return results;
         }
 
-        public async Task<UIImage> LoadImage(string imageUrl)
-        {
-            var httpClient = new HttpClient();
-            Task<byte[]> contentsTask = httpClient.GetByteArrayAsync(imageUrl);
-
-            // await! control returns to the caller and the task continues to run on another thread
-            var contents = await contentsTask;
-
-            // load from bytes
-            return UIImage.LoadFromData(NSData.FromArray(contents));
-        }
-
-
-
 
     }
 }
